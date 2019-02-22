045b505243ea3cfb9ee1c25a824db45e45ef232f
ZhangXiongWei <445080336@qq.com>
2019-02-22 18:17:51 +0800

    验证

--- src/api/login.js
+++ src/api/login.js
@@ -1,13 +1,17 @@
 import Ajax from '@/axios'
 //登录接口
-export const POST_LOGIN = async(payload) => {
-    return await Ajax.post('/users/login/?json', payload)
+export const POST_LOGIN = async (payload) => {
+  return await Ajax.post('/users/login/?json', payload)
 };
 //获取资产列表
-export const GET_Property = async() => {
-        return await Ajax.get('/property/house/?json&myproperty');
-    }
-    //重置密码
-export const GET_Password = async(params) => {
-    return await Ajax.get(`/users/emailscode/${params}?json`);
+export const GET_Property = async () => {
+  return await Ajax.get('/property/house/?json&myproperty');
+}
+//获取验证码
+export const GET_Password = async (params) => {
+  return await Ajax.get(`/users/emailscode/${params}?json`);
+}
+//重置密码
+export const SET_Password = async (payload) => {
+  return await Ajax.get(`/users/find_password/?json`, payload);
 }
--- src/views/login/Forgot.vue
+++ src/views/login/Forgot.vue
@@ -1,58 +1,78 @@
 <template>
   <div id="forgot">
-    <van-nav-bar
-      title="Reset Password"
-      left-arrow
-      @click-left="$router.go(-1)"
-      fixed
-      :border="false"
-    />
+    <van-nav-bar title="Reset Password" left-arrow @click-left="$router.go(-1)" fixed :border="false" />
     <div class="logo"></div>
     <h2>Need help with your password?</h2>
     <p>Enter the Email you use for Estrata, and you will receive a verfication code from us.</p>
-    <input v-model="email" type="text" class="common-inp">
-    <button @click="next" class="conmmt-btn">Next</button>
-    <div class="model" v-show="isShow">
-      <div>
-        <header>
-          <van-icon @click="isShow=!isShow" size=".15rem" name="cross"/>
-        </header>
-        <h3>Please enter into the verification code</h3>
-        <p>The verification code has been sent to your mobile phone.</p>
-        <security-code theme="line" placeholder v-model="code"></security-code>
-        <div class="sec">{{`30s`}}</div>
-      </div>
-    </div>
+    <template v-if="active==0">
+      <input v-model="email" type="text" class="common-inp">
+      <button @click="next" class="conmmt-btn">Next</button>
+    </template>
+    <template v-if="active==1">
+      <input v-model="code" type="text" class="common-inp" placeholder="Code">
+      <button @click="verify" class="conmmt-btn">Verify</button>
+    </template>
+    <template v-if="active==2">
+      <input v-model="password" type="text" class="common-inp" placeholder="New Password">
+      <button @click="reset" class="conmmt-btn">Reset</button>
+    </template>
+
   </div>
 </template>
 
 <script>
-import SecurityCode from "vue-security-code";
-import { GET_Password } from "@/api/login";
-export default {
-  components: { SecurityCode },
-  methods: {
-    next() {
-      GET_Password(this.email).then(res => {
-        if (res.data.status == "ok") {
-          this.$toast(res.data.msg);
-        } else {
-          this.$toast.fail(res.data.msg);
+  import SecurityCode from "vue-security-code";
+  import { GET_Password } from "@/api/login";
+  export default {
+    components: { SecurityCode },
+    methods: {
+      verify() {
+        GET_Password(`${this.email}/${this.code}`).then(res => {
+          if (res.data.status == "ok") {
+            this.$toast(res.data.msg);
+            this.active = 2;
+          } else {
+            this.$toast.fail(res.data.msg);
+          }
+        });
+      },
+      resete() {
+        let data = {
+          email: this.email,
+          password: this.password,
+          code: this.code,
         }
-      });
-      this.isShow = true;
+        SET_Password(data).then(res => {
+          if (res.data.status == "ok") {
+            this.$toast(res.data.msg);
+            this.$router.push('/login');
+          } else {
+            this.$toast.fail(res.data.msg);
+          }
+        });
+      },
+      next() {
+        GET_Password(this.email).then(res => {
+          if (res.data.status == "ok") {
+            this.$toast(res.data.msg);
+            this.active = 1;
+          } else {
+            this.$toast.fail(res.data.msg);
+          }
+        });
+      }
+    },
+    data() {
+      return {
+        code: "",
+        email: "",
+        isSend: false,
+        active: 0
+      };
     }
-  },
-  data() {
-    return {
-      isShow: false,
-      code: "",
-      email: ""
-    };
-  }
-};
+  };
 </script>
 
 <style lang="less" scoped>
-@import "./Forgot.less";
+  @import "./Forgot.less";
 </style>
